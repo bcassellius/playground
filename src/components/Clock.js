7082ed962e3a0bf2bfd950789426da0a08a43d54
Bri <bhilliker@gmail.com>
2021-11-12 21:34:55 -0600

    add date toggle functionality

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,14 +1,33 @@
+import moment from 'moment';
 import React, {Component} from 'react';
 import Clock from 'react-live-clock';
-// import { Switch } from 'antd';
-// import moment from 'moment';
+import { Switch } from 'antd';
 import "./Clock.css";
 
 class Time extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      todaysDate: false
+    }
+  }
+
+  toggleSwitch = () => {
+    this.setState({
+      todaysDate: !this.state.todaysDate
+    })
+  }
+
   render(){
     return(
       <div className="Time">
+        <div className="toggleButton">
+          <Switch onClick={this.toggleSwitch}/>
+        </div>
         <Clock format={'h:mm:ssa'} ticking={true} timezone={'US/Central'} />
+        <div className="toggleDate">
+          {this.state.todaysDate ? moment().format('MMMM Do YYYY') : ''}
+        </div>
       </div>
     );
   }
